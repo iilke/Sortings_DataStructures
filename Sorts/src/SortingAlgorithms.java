572e84ab69eb320c5ac313b773ff9a1548c6d283
İlke <117107449+iilke@users.noreply.github.com>
2023-11-05 21:03:04 +0300

    Merge Sort Implemented
    I have struggled in the recursive part of merge sort.

--- Sorts/src/SortingAlgorithms.java
+++ Sorts/src/SortingAlgorithms.java
@@ -2,7 +2,7 @@
 public class SortingAlgorithms {
     public static void main(String[] args) {
         int[] myArray = {2,8,3,5,6,7,1};
-        selectionSort(myArray);
+        mergeSort(myArray, 0, myArray.length-1);
         for(int i=0;i<myArray.length;i++){
             System.out.print(myArray[i] + " ");
         }
@@ -27,7 +27,47 @@ public class SortingAlgorithms {
 
     }
 
+    public static void mergeSort(int[] arr, int lo, int hi) {
+        if (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
 
+            mergeSort(arr, lo, mid);
+            mergeSort(arr, mid + 1, hi);
+
+            merge(arr, lo, mid, hi);
+        }
+    }
+
+
+    public static void merge(int[] arr, int lo, int mid, int hi){
+        int[] aux = new int[arr.length];
+
+        for(int i=0;i<arr.length;i++){
+            aux[i] = arr[i];              //creating a copy array
+        }
+
+        int i=lo;
+        int j=mid+1;
+        int k=lo;
+
+        while(i<=mid && j<=hi){
+            if(aux[j]<aux[i]){
+                arr[k++] = aux[j++];
+            }
+            else{
+                arr[k++] = aux[i++];
+            }
+        }
+
+        while(i<=mid){
+            arr[k++] = aux[i++];
+        }
+
+        while(j<=hi){
+            arr[k++] = aux[j++];
+        }
+
+}
 
 
     public static void swap(int[] arr, int i,int j){
